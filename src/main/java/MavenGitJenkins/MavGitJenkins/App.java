ab8d4a793479cc85baa111237fc8920693b1838e
sridharkasi <sridharan.kasi@gmail.com>
2017-04-24 01:13:17 +0530

    first checkin

---
+++ src/main/java/MavenGitJenkins/MavGitJenkins/App.java
@@ -0,0 +1,13 @@
+package MavenGitJenkins.MavGitJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/MavenGitJenkins/MavGitJenkins/TestRun.java
@@ -0,0 +1,11 @@
+package MavenGitJenkins.MavGitJenkins;
+
+import org.testng.annotations.Test;
+
+public class TestRun {
+	@Test
+	public void display(){
+		System.out.println("Welcome");
+	}
+
+}
